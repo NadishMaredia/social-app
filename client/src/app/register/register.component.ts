ec6b2d9ba70a2da72f5ab3f1c8facb2c13f350cf
Nadish Maredia <nd843857@dal.ca>
2021-12-25 15:33:41 -0400

    routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +12,8 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +23,8 @@ export class RegisterComponent implements OnInit {
       .subscribe(response => {
         console.log(response);
         this.cancel();
+      }, error => {
+        this.toastr.error(error.error);
       })
   }
 
